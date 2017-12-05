7d2394127f2635e061444e16c85abdf4935dcab9
Daniel J. Rollins <daniel@djrollins.com>
2017-12-05 23:27:29 +0000

    day 3.1: find bottom right corner and count back

--- 03-spiral-memory/spiral.h
+++ 03-spiral-memory/spiral.h
@@ -1,32 +1,30 @@
+#include <cmath>
 #include <cstdlib>
 
-// 37  36  35  34  33  32  31
-// 38  17  16  15  14  13  30
-// 39  18   5   4   3  12  29
-// 40  19   6   1   2  11  28
-// 41  20   7   8   9  10  27
-// 42  21  22  23  24  25  26
-// 43  44  45  46  47  48  49
+int side_length(int n)
+{
+	int root = std::ceil(std::sqrt(n));
+	return root % 2 ? root : root + 1;
+}
 
 int manhatten_distance(int n)
 {
 	if (n == 1)
 		return 0;
 
-	int current = 1;
-	int distance = 0;
-	int direction = -1;
-	int x = 0;
-	int y = 0;
+	int side = side_length(n);
+	int current = std::pow(side, 2);
+	int direction = 1;
+	int x = side / 2;
+	int y = -(side / 2);
 
-	while (current < n) {
-		distance += 1;
+	while (current > n) {
+		auto step = side - 1;
+		while (step-- && current > n) { --current; x -= direction; };
+		step = side - 1;
+		while (step-- && current > n) { --current; y += direction; };
+
 		direction *= -1;
-
-		auto step = distance;
-		while (--step && current < n) { ++current; x += direction; };
-		step = distance;
-		while (--step && current < n) { ++current; y += direction; };
 	}
 
 	return std::abs(x) + std::abs(y);
--- 03-spiral-memory/tests/spiral_test.cpp
+++ 03-spiral-memory/tests/spiral_test.cpp
@@ -1,6 +1,14 @@
 #include "../spiral.h"
 #include "catch.hpp"
 
+// 37  36  35  34  33  32  31
+// 38  17  16  15  14  13  30
+// 39  18   5   4   3  12  29
+// 40  19   6   1   2  11  28
+// 41  20   7   8   9  10  27
+// 42  21  22  23  24  25  26
+// 43  44  45  46  47  48  49
+
 TEST_CASE("can find relative coordinate for position 1")
 {
 	REQUIRE(0 == manhatten_distance(1));
@@ -14,6 +22,21 @@ TEST_CASE("can find manhatten distance for position 12")
 TEST_CASE("can find manhatten distance for position 23")
 {
 	REQUIRE(2 == manhatten_distance(23));
+}
+
+TEST_CASE("can find manhatten distance for position 24")
+{
+	REQUIRE(3 == manhatten_distance(24));
+}
+
+TEST_CASE("can find manhatten distance for position 25")
+{
+	REQUIRE(4 == manhatten_distance(25));
+}
+
+TEST_CASE("can find manhatten distance for position 26")
+{
+	REQUIRE(5 == manhatten_distance(26));
 }
 
 TEST_CASE("can find manhatten distance for position 1024")
