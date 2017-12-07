6286ed5da6442087f743e740f9d9b81c6df61e5f
Daniel J. Rollins <daniel@djrollins.com>
2017-12-07 00:14:14 +0000

    Hammer out days 3 through 6

--- 03-spiral-memory/tests/spiral_test.cpp
+++ 03-spiral-memory/tests/spiral_test.cpp
@@ -44,3 +44,66 @@ TEST_CASE("can find manhatten distance for position 1024")
 	REQUIRE(31 == manhatten_distance(1024));
 }
 
+#include <map>
+#include <iostream>
+
+class coord_generator
+{
+private:
+	int side_len = 1;
+	int section_len = side_len * 2;
+	int direction = 1;
+	std::pair<int, int> next{0, 0};
+public:
+	std::pair<int, int> operator()()
+	{
+		if (section_len-- > side_len)
+			next.first += direction;
+		else 
+			next.second += direction;
+
+		if (section_len == 0) {
+			direction *= -1;
+			side_len += 1;
+			section_len = side_len * 2;
+		}
+
+		return next;
+	}
+};
+
+TEST_CASE("dooot")
+{
+	coord_generator gen;
+
+	const int val = 265149;
+	const int size = std::ceil(std::pow(val, 1.0/5.0));
+	const int max_size = size % 2 == 0 ? size : size + 1;
+	const int centre = max_size / 2;
+
+	std::vector<std::vector<int>> grid(max_size);
+	for (auto &row : grid) row.resize(max_size, 0);
+	grid[centre][centre] = 1;
+
+	auto calculate_coord = [&grid, &centre](auto coord) {
+		const std::pair<int, int> neighbours[] = {
+			{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+
+		int sum = 0;
+
+		for (auto &neighbour : neighbours) {
+			sum += grid[coord.first + neighbour.first + centre][coord.second + neighbour.second + centre];
+		}
+
+		grid[coord.first + centre][coord.second + centre] = sum;
+
+		return sum;
+	};
+
+	int highest = 0;
+	while (highest <= val) {
+		highest = calculate_coord(gen());
+	}
+
+	REQUIRE(0 == highest);
+}
---
+++ 04-passphrases/passphrases.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using word_iter = std::istream_iterator<std::string>;
+
+int main()
+{
+	int count_unique = 0;
+	int count_anagram = 0;
+
+	std::string buffer;
+
+	while(std::getline(std::cin, buffer)) {
+		std::stringstream stream{buffer};
+
+		std::vector<std::string> words;
+		std::set<std::string> unique_words;
+		std::set<std::string> unique_anagrams;
+
+		std::copy(word_iter(stream), word_iter(), std::back_inserter(words));
+		std::copy(words.begin(), words.end(), std::inserter(unique_words, unique_words.end()));
+		std::transform(
+				words.begin(), words.end(),
+				std::inserter(unique_anagrams, unique_anagrams.end()),
+				[](auto str){ std::sort(str.begin(), str.end()); return str; });
+
+		if (words.size() == unique_words.size()) ++count_unique;
+		if (words.size() == unique_anagrams.size()) ++count_anagram;
+	}
+
+	std::cout << count_unique << '\n';
+	std::cout << count_anagram << '\n';
+}
