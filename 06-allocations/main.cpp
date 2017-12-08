9952cc498de61288313d8dac625f2f0b443e91c8
Daniel J. Rollins <daniel@djrollins.com>
2017-12-08 19:00:31 +0000

    add solution for day 6

---
+++ 06-allocations/main.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <vector>
+
+template<typename BeginIter, typename EndIter>
+void reallocate(BeginIter first, EndIter last)
+{
+	auto bank = std::max_element(first, last);
+	auto blocks = *bank;
+	*bank = 0;
+
+	while(blocks--) {
+		++bank;
+		if (bank == last) bank = first;
+		++(*bank);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	std::vector<int> banks;
+	std::transform(argv + 1, argv + argc, std::back_inserter(banks), std::atoi);
+	std::vector<std::vector<int>> history{banks};
+
+	int count = 0;
+	long cycles = 0;
+
+	for (bool uniq = true; uniq; ++count) {
+		reallocate(banks.begin(), banks.end());
+		auto found = std::find(history.cbegin(), history.cend(), banks);
+		cycles = history.end() - found;
+		uniq = found == history.end();
+		history.push_back(banks);
+	}
+
+	std::cout << count << '\n';
+	std::cout << cycles << '\n';
+}
