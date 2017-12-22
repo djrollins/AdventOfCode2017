f4f1bbe852f522949e72e761e7f7d0dd5602a6d1
Daniel J. Rollins <daniel@djrollins.com>
2017-12-22 15:05:46 +0000

    complete day 11 part 1

---
+++ 11-hex-ed/main.cpp
@@ -0,0 +1,62 @@
+#include <numeric>
+#include <cmath>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <unordered_map>
+
+struct coord
+{
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	coord &operator+=(const coord &rhs)
+	{
+		x += rhs.x;
+		y += rhs.y;
+		z += rhs.z;
+		return *this;
+	}
+};
+
+static const std::unordered_map<std::string, coord> dirs = {
+	{"n",  {0, 1, -1}},
+	{"ne", {1, 0, -1}},
+	{"se", {1, -1, 0}},
+	{"s",  {0, -1, 1}},
+	{"sw", {-1, 0, 1}},
+	{"nw", {-1, 1, 0}}
+};
+
+
+int distance(const coord &from, const coord &to)
+{
+	using std::abs;
+	return (abs(from.x - to.x) + abs(from.y - to.y) + abs(from.z - to.z)) / 2;
+}
+
+void move(coord &coord, std::string const &dir)
+{
+	coord += dirs.at(dir);
+}
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	std::regex splitter{"(ne|se|sw|nw|n|s)"};
+	std::string input{argv[1]};
+	std::sregex_iterator begin{input.begin(), input.end(), splitter};
+	std::sregex_iterator end{};
+
+	coord dest = std::accumulate(
+			begin, end,
+			coord{},
+			[](auto acc, auto &dir) {
+				move(acc, dir.str());
+				return acc;
+			});
+
+	std::cout << distance(coord{}, dest) << '\n';
+}
